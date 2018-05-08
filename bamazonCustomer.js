ca01a16735fe448b8ec4c5a7f77b7a33f8576992
weshirk <whitney.shirk@gmail.com>
2018-05-08 18:14:36 -0500

    Add files via upload

---
+++ bamazonCustomer.js
@@ -0,0 +1,120 @@
+// Pull in required dependencies
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+// Define the MySQL connection parameters
+var connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+
+	// Your username
+	user: 'root',
+
+	// Your password
+	password: '',
+	database: 'bamazon'
+});
+
+// promptUserPurchase will prompt the user for the item/quantity they would like to purchase
+function userPurchase() {
+
+	inquirer.prompt([
+		{
+			type: 'input',
+			name: 'item_id',
+			message: 'Please enter the Item ID which you would like to purchase.',
+			validate: validateInput,
+			filter: Number
+		},
+		{
+			type: 'input',
+			name: 'quantity',
+			message: 'How many do you need?',
+			validate: validateInput,
+			filter: Number
+		}
+	]).then(function(input) {
+
+		var item = input.item_id;
+		var quantity = input.quantity;
+
+		var queryStr = 'SELECT * FROM products WHERE ?';
+
+		connection.query(queryStr, {item_id: item}, function(err, data) {
+			if (err) throw err;
+
+
+
+			if (data.length === 0) {
+				console.log('ERROR: invalid');
+				displayInventory();
+
+			} else {
+                var productData = data[0];
+                
+             
+				if (quantity <= productData.stock_quantity) {
+					console.log('we have the product you requested in stock');
+
+					var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+				
+
+					connection.query(updateQueryStr, function(err, data) {
+						if (err) throw err;
+
+						console.log('Your order has been placed! Your total is $' + productData.price * quantity);
+						console.log('Thank you for shopping with us!');
+						console.log("\n--------------\n");
+
+			
+						connection.end();
+					})
+				} else {
+					console.log('Sorry, there is not enough product in stock, your order can not be placed as is.');
+					console.log('Please modify your order.');
+					console.log("\n-----------------\n");
+
+					displayInventory();
+				}
+			}
+		})
+	})
+}
+
+
+function displayInventory() {
+
+	queryStr = 'SELECT * FROM products';
+
+	connection.query(queryStr, function(err, data) {
+		if (err) throw err;
+
+		console.log('Existing Inventory: ');
+		console.log('...................\n');
+
+		var strOut = '';
+		for (var i = 0; i < data.length; i++) {
+			strOut = '';
+			strOut += 'Item ID: ' + data[i].item_id + '  //  ';
+			strOut += 'Product Name: ' + data[i].product_name + '  //  ';
+			strOut += 'Department: ' + data[i].department_name + '  //  ';
+			strOut += 'Price: $' + data[i].price + '\n';
+
+			console.log(strOut);
+		}
+
+	  	console.log("------------------------\n");
+
+
+	  	promptUserPurchase();
+	})
+}
+
+
+function runBamazon() {
+
+	displayInventory();
+}
+
+
+runBamazon();
